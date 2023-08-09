269c9b445ad7a8ec7a2d22ab06db9d2fff57579b
S4nt1nO <Gianluca1996@gmx.de>
2023-08-09 10:53:35 +0200

    done

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -2,8 +2,17 @@ import AllBeers from '../../../assets/img/Group 4.png'
 import RandomBeer from '../../../assets/img/Group 5.png'
 import './Home.scss'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { apiLink } from '../../data/data.js'
 
 function Home() {
+  const [randomBeer, setRandomBeer] = useState([])
+  useEffect(() => {
+    fetch(`${apiLink}/random`)
+      .then(response => response.json())
+      .then(response => setRandomBeer(response))
+  }, [])
+
   return (
     <div>
       <section className={'landing_section'}>
@@ -17,7 +26,9 @@ function Home() {
           </p>
         </article>
         <article>
-          <img src={RandomBeer} alt="" />
+          <Link to={`/details/${randomBeer._id}`}>
+            <img src={RandomBeer} alt="" />
+          </Link>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo illo itaque soluta. Ab aliquid corporis
             explicabo maiores nulla vitae voluptatibus.
